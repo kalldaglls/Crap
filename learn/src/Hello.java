50d64b3b827d5a816f6de1637f6713f2b02e9926
kalldaglls <kalldaglls@mail.ru>
2020-07-23 20:50:03 +0300

    23/07/20 20:47. Программа работает, но есть баги: 1) Если загаданное слово pea, а я ввел peanut, то выдает "peaУгадайте слово!" 2)Угадайте слово после ошибки должно быть на след строке! 2.1 Решено частично: если последний символ неверный, то переход идет. Если последний верный, то нет перехода 3) Если слово загаданное больше или наоборот, то выдает ошибку, что превышен лимит. 3.1 Решено через цикл while с двумя условиями+++ 4) Как показывать пользователю отгаданные буквы, каждый раз не уберая предыдущие отгаданные?

--- learn/src/Hello.java
+++ learn/src/Hello.java
@@ -12,22 +12,24 @@ public class Hello {
         int i = random.nextInt(array.length - 1);
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("============================");
+            System.out.println("===========================");
             System.out.println("Угадайте слово!");
             String b = scanner.nextLine();
-            if (array[i].equals(b)) {//с == не работала!
+            if (array[i].equals(b)) {//- с == не работала! - здесь через || нужно прописать условие: если введенное слово не эквивалентно загаданному, но полностью его содержит, то оно считается угаданным!
                 System.out.println("Вы угадали!");
                 return array[i];
             } else {
                 //for (int j = 0; j <= array[i].length() - 1; j++) {
                 int j = 0;
-                while ((j <= array[i].length() - 1) && (b.length() >= j)) {
-                    if (array[i].charAt(j) == b.charAt(j)) {//что делать, если загаданное число больще, чем введенное(дает ошибку out of range), а мы хотим дальше писать ##?
+                while ((j <= array[i].length() - 1) && (b.length() - 1 >= j)) {
+                    if (array[i].charAt(j) == b.charAt(j)) {//что делать, если загаданное число больще, чем введенное(дает ошибку out of range), а мы хотим дальше писать ##? Решено!
                         System.out.print(array[i].charAt(j));
-                    } else System.out.print("###");
+                    } else {
+                        if (j < array[i].length() - 1) System.out.print("###");
+                        else System.out.println("###");
+                    }
                     j++;
                 }
-                System.out.println("============================");
             }
         }
     }
